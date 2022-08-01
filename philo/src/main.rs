f405240107afe262b4d17c2f32c83d749e172f77
Leo-Gyger <gygerleo@proton.me>
2022-08-01 18:17:46 +0200

    Added a basic test mutex

--- philo/src/main.rs
+++ philo/src/main.rs
@@ -1,10 +1,12 @@
 use std::env;
 use std::process::exit;
 use std::thread;
+use std::sync::{Arc, Mutex};
 pub mod rules;
 
-fn test(nbr: i32) 
+fn test(nbr: i32, data: Arc<Mutex<i32>>)
 {
+    data.lock().unwrap();
     for i in 1..10
     {
         println!("test {} {} ", nbr, i);
@@ -12,14 +14,23 @@ fn test(nbr: i32)
 }
 
 fn main() {
+    let mut handles = vec![];
     let args: Vec<String> = env::args().collect();
     if args.len() < 5 || args.len() > 6
         {
             println!("error argument");
             exit(1);
         }
+    let data = Arc::new(Mutex::new(0));
     let _stru = rules::init_rules(args);
-    thread::spawn(|| test(0));
-    let handle = thread::spawn(|| test(1));
-    handle.join().unwrap();
+    for i in 0..2
+    {
+        let data = Arc::clone(&data);
+        let handle = thread::spawn(move || test(i,data));
+        handles.push(handle);
+    }
+    for handle in handles
+    {
+        handle.join().unwrap();
+    }
 }
